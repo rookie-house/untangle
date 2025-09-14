9b4f588d06f1c6839aef39bbe3c9b8766c0c66f9
AdityaTote <adityatote23@gmail.com>
2025-09-14 16:18:33 +0530

    hotfix: final 3 cd push

--- apps/server/src/routes/documents.routes.ts
+++ apps/server/src/routes/documents.routes.ts
@@ -1,6 +1,6 @@
-import { hono } from "@/lib/hono";
-import { DocumentsController } from "@/controller";
-import { authMiddleware } from "@/middleware/auth.middleware";
+import { hono } from '@/lib/hono';
+import { DocumentsController } from '@/controller';
+import { authMiddleware } from '@/middleware/auth.middleware';
 
 const app = hono();
 
@@ -10,6 +10,5 @@ app.put('/upload', DocumentsController.upload);
 app.get('/all', DocumentsController.getDocuments);
 app.get('/:id', DocumentsController.getDocumentById);
 app.get('/session/:sessionId', DocumentsController.getDocumentsBySession);
-
 
 export default app;
