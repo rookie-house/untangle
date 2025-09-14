2dfebf24c5d4696457f2622259c2199b7583e07b
AdityaTote <adityatote23@gmail.com>
2025-09-14 07:03:31 +0530

    feat: r2 bucket and session schema

---
+++ apps/server/src/lib/db/schema/document.schema.ts
@@ -0,0 +1,44 @@
+import { relations } from 'drizzle-orm';
+import { index, int, sqliteTable, text, uniqueIndex } from 'drizzle-orm/sqlite-core';
+import { users } from './user.schema';
+import { sessions } from './session.schema';
+
+export const documents = sqliteTable(
+	'documents',
+	{
+		id: int().notNull().primaryKey({ autoIncrement: true }),
+		title: text().notNull(),
+		type: text('type', {
+			enum: ['image', 'pdf', 'other'],
+		})
+			.$type<'image' | 'pdf' | 'other'>()
+			.notNull(),
+		r2_key: text().notNull(),
+		url: text().notNull(),
+		userId: int().notNull(),
+		sessionId: int().notNull(),
+		updatedAt: int({ mode: 'timestamp' })
+			.notNull()
+			.$defaultFn(() => new Date())
+			.$onUpdateFn(() => new Date()),
+		createdAt: int({ mode: 'timestamp' })
+			.notNull()
+			.$defaultFn(() => new Date()),
+	},
+	(t) => [
+		uniqueIndex('users_sessionId_idx').on(t.sessionId),
+		index('users_title_idx').on(t.title),
+		index('users_userId_idx').on(t.userId),
+		index('users_type_idx').on(t.type),
+		index('users_title_idx').on(t.title),
+	],
+);
+
+export const documentRelations = relations(documents, ({ one }) => ({
+	user: one(users),
+	session: one(sessions),
+}));
+
+export type IDocument = typeof documents.$inferSelect;
+export type IPartialDocument = Partial<typeof documents.$inferInsert>;
+export type INewDocument = typeof documents.$inferInsert;
--- apps/server/src/lib/db/schema/user.schema.ts
+++ apps/server/src/lib/db/schema/user.schema.ts
@@ -1,22 +1,35 @@
-import {
-	int,
-	sqliteTable,
-	text,
-	uniqueIndex,
-} from "drizzle-orm/sqlite-core";
+import { relations } from 'drizzle-orm';
+import { index, int, sqliteTable, text, uniqueIndex } from 'drizzle-orm/sqlite-core';
+import { sessions } from './session.schema';
+import { documents } from './document.schema';
 
 export const users = sqliteTable(
-	"users",
+	'users',
 	{
 		id: int().primaryKey({ autoIncrement: true }),
-		email: text().notNull().unique(),
+		name: text(),
+		email: text().notNull(),
 		password: text(),
-		googleAccess: text(),
+		profilePic: text(),
+		google_access_token: text(),
+		updatedAt: int({ mode: 'timestamp' })
+			.notNull()
+			.$defaultFn(() => new Date())
+			.$onUpdateFn(() => new Date()),
+		createdAt: int({ mode: 'timestamp' })
+			.notNull()
+			.$defaultFn(() => new Date()),
 	},
 	(t) => [
-		uniqueIndex("users_email_idx").on(t.email),
-	]
+		uniqueIndex('users_email_idx').on(t.email),
+		index('users_name_idx').on(t.name),
+	],
 );
+
+export const userRelations = relations(users, ({ many }) => ({
+	session: many(sessions),
+	documents: many(documents),
+}));
 
 export type IUser = typeof users.$inferSelect;
 export type IPartialUser = Partial<typeof users.$inferInsert>;
