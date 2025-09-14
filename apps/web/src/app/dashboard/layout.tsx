db2e9648498ef943733589843bd20331983187c4
Harsh-Duche <ducheharsh@gmail.com>
2025-09-15 00:05:45 +0530

    fix: build issues

--- apps/web/src/app/dashboard/layout.tsx
+++ apps/web/src/app/dashboard/layout.tsx
@@ -1,9 +1,9 @@
 import { ReactNode } from 'react'
 
 export default function Layout({
-  children,
+    children,
 }: Readonly<{
-  children: ReactNode;
+    children: ReactNode;
 }>) {
     return (
         <div className="min-h-screen flex flex-col">
